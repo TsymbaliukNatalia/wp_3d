7504d09c8d5701327b48559ac2a6c9ef88826d1c
TsymbaliukNatalia <tsymbaliuk.natalia20@gmail.com>
2021-04-30 17:55:18 +0300

    add remove method for order array

--- wp-content/plugins/my-order-plagin/js/order-script.js
+++ wp-content/plugins/my-order-plagin/js/order-script.js
@@ -27,4 +27,62 @@ jQuery(document).ready(function($){
             });  
         });
     });
+
+    $("#bulk-action-selector-top").change(function(){
+        if($(this).val() == 'bulk-delete'){
+            let delete_orders_array = [];
+            let delete_rows = [];
+            $('.check-column input[type=checkbox]:checked').each(function(index, element) {
+                let delete_id = $(this).parent().next('.id').text();
+                delete_id = parseInt(delete_id);
+                delete_rows.push(element.closest('tr'));
+                delete_orders_array.push(delete_id); 
+            });
+            if(delete_orders_array.length > 0){
+                $('#popup-delete-order').css('display', 'block');
+                $('#delete-order-no').on('click', function(e){
+                    $('#popup-delete-order').css('display', 'none');
+                });
+                $('#delete-order-yes').on('click', function(e){
+                    $('#popup-delete-order').css('display', 'none');
+                    $.ajax({
+                        url : ajaxurl + '?action=ajax_admin_bulk_delete_order',
+                        type: 'POST',
+                        data: {
+                            data: delete_orders_array,},
+                        success:function(result){
+                            delete_rows.forEach(function(element){
+                                element.remove();
+                            });
+                        }
+                    });  
+                });
+            }
+           
+        }
+    
+    });
+
+    // $('.delete_order').on('click', function(e){
+    //     e.preventDefault();
+    //     let delete_id = e.target.getAttribute('data-id');
+    //     let delete_row = e.target.closest('tr');
+    //     $('#popup-delete-order').css('display', 'block');
+       
+    //     $('#delete-order-no').on('click', function(e){
+    //         $('#popup-delete-order').css('display', 'none');
+    //     });
+    //     $('#delete-order-yes').on('click', function(e){
+    //         $('#popup-delete-order').css('display', 'none');
+    //         $.ajax({
+    //             url : ajaxurl + '?action=ajax_admin_delete_order',
+    //             type: 'POST',
+    //             data: {
+    //                 data: delete_id,},
+    //             success:function(result){
+    //                 delete_row.remove();
+    //             }
+    //         });  
+    //     });
+    // });
 });
--- wp-content/plugins/my-order-plagin/my-order-functions.php
+++ wp-content/plugins/my-order-plagin/my-order-functions.php
@@ -1,14 +1,14 @@
 <?php
-function sunset_load_admin_scripts(){ 
+function order_load_admin_scripts(){ 
     wp_enqueue_media();
-    wp_register_script('sunset-admin-script', plugins_url('my-order-plagin/js/order-script.js'), array('jquery'), '1.0.0', true);
-    wp_enqueue_script('sunset-admin-script'); 
+    wp_register_script('order-admin-script', plugins_url('my-order-plagin/js/order-script.js'), array('jquery'), '1.0.0', true);
+    wp_enqueue_script('order-admin-script'); 
 }
-add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts' );
+add_action( 'admin_enqueue_scripts', 'order_load_admin_scripts' );
 
 if (wp_doing_ajax()) {
-    // add_action('wp_ajax_nopriv_ajax_admin_delete_order', 'ajax__admin_delete_order');
     add_action('wp_ajax_ajax_admin_delete_order', 'ajax_admin_delete_order');
+    add_action('wp_ajax_ajax_admin_bulk_delete_order', 'ajax_admin_bulk_delete_order');
 };
 
 function ajax_admin_delete_order()
@@ -18,44 +18,17 @@ function ajax_admin_delete_order()
     $res = $wpdb->delete( 'wp_order', [ 'id' => $delete_id ] );
     echo json_encode($res);
 	wp_die();
-	// $data = $_POST['data'];
-	// $customer = $_POST['data'][0];
-	// $orders = $_POST['data'][1];
-	// $pay_sum = 0;
 
-	// foreach ($orders as $order) {
-	// 	$pay_sum += $order[2];
-	// }
-	
+}
+function ajax_admin_bulk_delete_order()
+{
+    global $wpdb;
+    $delete_id = $_POST['data'];
+    foreach($delete_id as $id){
+        $wpdb->delete( 'wp_order', [ 'id' => $id ] ); 
+    }
+    // $res = $wpdb->delete( 'wp_order', [ 'id' => $delete_id ] );
+    // echo json_encode($_POST['data']);
+	wp_die();
 
-	// if(isset($customer) && isset($pay_sum) && isset($orders)){
-	// 	$res1 = $wpdb->insert( 'wp_order', [
-	// 		'first_name' => $customer[0],
-	// 		'last_name' => $customer[1],
-	// 		'email' => $customer[2],
-	// 		'phone_number' => $customer[3],
-	// 		'city' => $customer[4],
-	// 		'to_pay' => $pay_sum 
-	// 		] );
-	// 	$last_insert_id = $wpdb->insert_id;
-	// 	$res2 = true;
-	// 	foreach ($orders as $order) {
-	// 		$res3 = $wpdb->insert( 'wp_order_product', [
-	// 			'id_order' => $last_insert_id,
-	// 			'id_product' => $order[0],
-	// 			'quantity' => $order[1],
-	// 			'cost' => $order[2]
-	// 		]);
-	// 		if($res3 != true){
-	// 			$res2 = false;
-	// 		}
-	// 	}
-	// };
-	// if($res1 && $res2){
-	// 	echo json_encode($res1);
-	// 	wp_die(); 
-	// } else {
-	// 	echo json_encode(false);
-	// 	wp_die();
-	// }
 }
